132b78e68d2dc5b58718e6ebb99aacfb3950d926
zhihan <zhi.han@gmail.com>
2014-10-06 10:12:18 -0400

    Added test for toStringHelper

--- src/trygroovy/GuavaObjectMethodTest.groovy
+++ src/trygroovy/GuavaObjectMethodTest.groovy
@@ -18,10 +18,19 @@ public class GuavaObjectMethodTest {
       .compare(this.lastName, that.lastName)
       .result();
     }
+    
+    @Override
+    String toString() {
+      return Objects.toStringHelper(this)
+      .add("firstName", firstName)
+      .add("lastName", lastName)
+      .toString();
+    }
   }
   
   @Test
   public void testEquals() {
+    // Test the robust equal method
     assertTrue(Objects.equal("a", "a"))
     assertFalse(Objects.equal(null, "a"))
     assertFalse(Objects.equal("a", null))
@@ -30,8 +39,19 @@ public class GuavaObjectMethodTest {
   
   @Test
   public void testComparisonChain() {
+    // Test use the comparison chain.
     People a = new People(firstName: "a", lastName: "b")
     People b = new People(firstName: "b", lastName: "c")
     assertTrue(a.compareTo(b) != 0)
+    assertTrue(a.compareTo(a) == 0)
+  }
+  
+  @Test
+  public void testToString(){
+    // Try to use the toStringHelper
+    People a = new People(firstName: "Natalie", lastName: "Han")
+    String s = a.toString()
+    System.out.println(s) // Print the toString to console
+    assertTrue(s.length() > 10)
   }
 }
